8ba1be3fffd7d8055094d12a51cbbe2dac362223
h21org <h21sorg@gmail.com>
2024-12-04 01:20:58 +0330

    Update download_and_delete.sh

--- download_and_delete.sh
+++ download_and_delete.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
-# لینک فایل بزرگ برای دانلود
-url="https://speed.hetzner.de/10GB.bin" # تغییر دهید به فایل دلخواه
+# لینک فایل
+URL="https://dla.p30day.ir/movie/98-09/J-r.2019.2160p.UHD.BluRay.x265-AAAUHD-www.P30Day.com.mkv?66499"
+
+# نام فایل موقت
+TEMP_FILE="temp_file.mkv"
 
 # تعداد دفعات دانلود
-download_count=10
+DOWNLOAD_COUNT=100  # تعداد دفعات دانلود دلخواه را تنظیم کنید
 
-# دانلود و حذف فایل‌ها
-for ((i=1; i<=download_count; i++))
-do
-  echo "در حال دانلود فایل شماره $i..."
-  curl -o /dev/null "$url"
-  
-  if [ $? -eq 0 ]; then
-    echo "دانلود موفقیت‌آمیز بود."
-  else
-    echo "خطا در دانلود."
-    break
-  fi
+for ((i=1; i<=DOWNLOAD_COUNT; i++)); do
+    echo "دانلود فایل شماره $i از $DOWNLOAD_COUNT ..."
+    
+    # دانلود فایل
+    curl -L -o "$TEMP_FILE" "$URL"
+
+    if [ $? -eq 0 ]; then
+        echo "دانلود کامل شد. حذف فایل ..."
+        rm -f "$TEMP_FILE"
+    else
+        echo "خطایی در دانلود رخ داد! دانلود متوقف می‌شود."
+        break
+    fi
 done
 
-echo "دانلود به پایان رسید."
+echo "دانلود و حذف فایل‌ها به پایان رسید."
